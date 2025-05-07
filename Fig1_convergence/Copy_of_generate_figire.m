015f5feca57b2bd8846291aff1690b7231fbbe6f
Tianyu Fang <118038690+Nostalgia2022@users.noreply.github.com>
2025-05-07 16:24:08 +0300

    Add files via upload

---
+++ Fig1_convergence/Copy_of_generate_figire.m
@@ -0,0 +1,37 @@
+clc
+clear
+
+load data_ML_SGPI_iter40_snr20.mat
+
+Y1=squeeze(mean(Convergence_tot));
+
+load data_SGPI_dv_snr20.mat
+
+Y2=squeeze(mean(Convergence_tot));
+
+load data_SGPI_Ger_snr20.mat
+
+Y3=squeeze(mean(Convergence_tot));
+load data_SGPI_fixed_snr20.mat
+
+Y4=squeeze(mean(Convergence_tot));
+
+
+figure;
+for i = 1:6
+    subplot(2,3,i); % Create a 2x3 grid of subplots
+    plot(Y1(:,i), '-', 'Color',color(1),'LineWidth', 1.5); % Plot Y1 in blue
+    hold on;
+    plot(Y2(:,i), '--', 'Color',color(2),'LineWidth', 1.5); % Plot Y2 in red dashed line
+    hold on;
+    plot(Y3(:,i),'-.','Color',color(3),'LineWidth', 1.5);
+
+    plot(Y4(:,i),'-','Color',color(4),'Linewidth',1.5)
+
+    xlabel('Iteration Index');
+    ylabel(['Objective Value']);
+    
+    legend('SGPI-Deep', 'SGPI-Eig','SGPI-Gersh', 'SGPI-Fixed','Location','southeast');
+    grid on;
+end
+    
